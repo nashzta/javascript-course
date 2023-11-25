073af45a848004452cffd929a26072fd80b72279
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2023-11-25 11:13:54 -0500

    basic operators, precedence operators, template strings

---
+++ 01-Fundamentals-Part-1/challenges.js
@@ -0,0 +1,56 @@
+/* //*CHALLENGE #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
+
+Your task is to write some code to help them:
+
+Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
+
+Calculate both their BMIs using the formula, and store the results in two variables called BMIMark and BMIJohn.
+
+Log the value of BMIMark and BMIJohn to the console.
+
+BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a higher BMI than John. Log it to the console too
+
+TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+
+
+👋 OPTIONAL: You can watch my solution in video format in the next lecture
+
+
+
+IMPORTANT: The ** operator is not supported in this editor. Please make sure to use exactly this formula mass / (height * height), and not this one mass / (height ** 2). */
+
+//*- Solution Jean
+// let massMark, heightMark;
+// let massJohn, heightJohn;
+// let BMIMark, BMIJohn;
+// let markHigherBMI;
+
+// massMark = 78;
+// heightMark = 169;
+
+// massJohn = 92;
+// heightJohn = 195;
+
+// BMIMark = massMark / (heightMark * heightMark);
+// BMIJohn = massJohn / (heightJohn * heightJohn);
+
+// console.log(BMIMark);
+// console.log(BMIJohn);
+
+//*- Solution Jonas
+
+// const massMark = 78;
+// const heightMark = 1.69;
+
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
+
+// console.log(BMIMark);
+// console.log(BMIJohn);
+// console.log(markHigherBMI);
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,25 +1,154 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+//* Variables and Value
 
-console.log("Jonas");
-console.log(23);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-let firstName = "Matilda";
+// console.log("Jonas");
+// console.log(23);
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Matilda";
 
-let jonas_matilda = "JM";
-let $function = "27";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let person = "Jonas";
-let PI = 3.1415;
+// // Variable name conventions
+// let jonas_matilda = "JM";
+// let $function = "27";
 
-let myFirstJob = "Programmer";
-let myCurrentJob = "Teacher";
+// let person = "Jonas";
+// let PI = 3.1415;
 
-let job1 = "programmer";
-let job2 = "teacher";
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
 
-console.log(myFirstJob);
+// let job1 = "programmer";
+// let job2 = "teacher";
+
+// console.log(myFirstJob);
+
+//* Data Types
+
+// // Strings
+// console.log("Hola, soy Jean Pierre", typeof "Hola, soy Jean Pierre");
+
+// // Number
+// console.log(2023, typeof 2023);
+
+// // Boolean
+// console.log(true, typeof true);
+
+// // Dynamic Typing
+
+// let dynamicVariable = "Hola, soy Texto";
+
+// console.log("Dynamic variable:", dynamicVariable, typeof dynamicVariable);
+
+// dynamicVariable = 2023;
+
+// console.log("Dynamic variable:", dynamicVariable, typeof dynamicVariable);
+
+// // Undefined
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+//* Basic Operators
+
+// // Math Operators
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+// const firstName = "Jonas";
+// const lastName = "Schmedtmann";
+// console.log(firstName + " " + lastName);
+
+// // Assignment Operators
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x++;
+// x--;
+// x--;
+
+// console.log(x);
+
+// // Comparison Operators
+
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018);
+
+//* Operators procedence
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+// let x, y;
+
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
+
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah, averageAge);
+
+//* Strings and Template Literals
+
+// const firstName = "Jean";
+// const year = 2023;
+// const birthYear = 2000;
+// const job = "stevedore";
+
+// const jean =
+//   "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+// console.log(jean);
+
+// const jeanNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+// console.log(jeanNew);
+
+// console.log(`Just a regular string...`);
+
+// console.log(
+//   "String with \n\
+// multiple \n\
+// lines"
+// );
+
+// console.log(`String
+// with
+// multiple
+// lines`);
+
+//* Taking Decisions: if/else statements
+
+// const age = 19;
+
+// if (age >= 18) {
+//   console.log("Sarah can star driving license 🚘");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+// }
+
+// const birthYear = 1998;
+
+// let century;
+
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+
+// console.log(century);
