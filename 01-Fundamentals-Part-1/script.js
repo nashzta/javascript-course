a6cd71357f7aa4bc4dea2705049b612d5d0d8d66
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2023-11-26 10:44:48 -0500

    coercion y conversion, operadores de igualdad, logica booleana

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -152,3 +152,71 @@
 // }
 
 // console.log(century);
+
+//* Type Conversion and Coercion
+
+// // Conversion
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number("Jean"));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// // Coercion
+
+// console.log("I am " + 23 + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" / "2");
+
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
+
+//* Truthy and Falsy values
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Jonas"));
+// console.log({});
+// console.log("");
+
+// const money = 100;
+// if (money) {
+//   console.log("Don't spend it all ;)");
+// } else {
+//   console.log("You should get a job!");
+// }
+
+// let height = 0;
+// if (height) {
+//   console.log("YAY! Height is defined");
+// } else {
+//   console.log("Height is UNDEFINED");
+// }
+
+//* Equality Operators
+
+// const age = "18";
+// if (age === 18) console.log("You just became an adult :D (strict)");
+
+// if (age === 18) console.log("You just became an adult :D (loose)");
+
+// const favourite = Number(prompt("What's your favourite number?"));
+
+// console.log(favourite);
+// console.log(typeof favourite);
+
+// if (favourite === 23) {
+//   console.log("Cool! 23 is an amazing number!");
+// } else if (favourite === 7) {
+//   console.log("7 is also a cool number");
+// } else if (favourite === 9) {
+//   console.log("9 is also a cool number");
+// } else {
+//   console.log("Number is not 23 or 7");
+// }
+
+// if (favourite !== 23) console.log("Why not 23?");
