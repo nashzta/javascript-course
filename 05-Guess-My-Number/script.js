696052d3bcb5536cfaf2f878d2b00427c8bc7373
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2023-12-02 21:06:36 -0500

    what is dom, manipulating dom and handling event

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+//* Selecting and manipulatin elements
+
+// console.log(document.querySelector('.message').textContent);
+// const message = (document.querySelector('.message').textContent =
+//   '🎉 Correct Number!');
+// console.log(message);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 22;
+// console.log(document.querySelector('.guess').value);
+
+//* Handling click events
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = document.querySelector('.guess').value;
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number';
+  }
+});
+
+//* Implemeting the game logic
