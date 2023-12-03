6c70adeb5fa593b7af9709ff877486870d877ad6
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2023-12-03 17:41:52 -0500

    manipulating styles, principle dry, guess-my-number project finished

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -15,13 +15,62 @@
 
 //* Handling click events
 
-document.querySelector('.check').addEventListener('click', () => {
-  const guess = document.querySelector('.guess').value;
-  console.log(typeof guess);
+// document.querySelector('.check').addEventListener('click', () => {
+//   const guess = document.querySelector('.guess').value;
+//   console.log(typeof guess);
+
+//   if (!guess) {
+//     document.querySelector('.message').textContent = '⛔ No Number';
+//   }
+// });
+
+//* Implemeting the game logic
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No Number';
+    displayMessage('⛔ No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    highScore = highScore < score ? score : highScore;
+    document.querySelector('.highscore').textContent = highScore;
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess < secretNumber ? '📉 Too Low!' : '📈 Too high!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lose the game!');
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
 
-//* Implemeting the game logic
+function resetGame() {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = null;
+  document.querySelector('.score').textContent = 20;
+  displayMessage('Start guessing...');
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+}
+
+document.querySelector('.again').addEventListener('click', resetGame);
+
+// Coding Challenge #1
