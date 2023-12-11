9fb6b8250128ba86d6f9d910c83d843219d7c168
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2023-12-11 09:50:07 -0500

    scope concepts, scope chaing, scope practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,3 +1,34 @@
 'use strict';
 
 //* An High-Level Overview of JavaScript
+
+//* Scope and the Scope Chain
+
+//* Scope in practice
+
+function calcAge(birthYear) {
+  const age = 2023 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'Pierre';
+      const str = `Oh, and you're a millenial ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(millenial);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jean';
+calcAge(1990);
