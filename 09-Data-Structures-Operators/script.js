99cb329a90e76ef976d67fb87a8c315ac6a1da6c
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2024-01-05 20:54:07 -0500

    optional chainging, challenge 2

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -64,7 +64,63 @@ const restaurant = {
   },
 };
 
+//* Looping objects: Object keys, values and entries
+
+// Property NAMES
+// const properties = Object.keys(openingHours);
+// console.log(properties);
+
+// let openStr = `We are open on ${properties.length} days: `;
+
+// for (const day of Object.keys(openingHours)) {
+//   openStr += `${day}, `;
+// }
+// console.log(openStr);
+
+// // Property VALUES
+
+// const values = Object.values(openingHours);
+// console.log(values);
+
+// // Entire Object
+
+// const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// for (const [key, { open, close }] of entries) {
+//   console.log(`On ${key} we open at ${open} and close at ${close}`);
+// }
+
+//* Optional Chaining (?.)
+
+// if (restaurant.openingHours && restaurant.openingHours.mon)
+//   console.log(restaurant.openingHours.mon.open);
+
+// //with optional chaining
+// console.log(restaurant.openingHours?.mon?.open);
+
+// //Example
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+// for (const day of days) {
+//   const open = restaurant.openingHours[day]?.open ?? 'closed';
+//   console.log(`On ${day}, we open at ${open}`);
+// }
+
+// // Methods
+
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+// console.log(restaurant.orderRissoto?.(0, 1) ?? 'Method does not exist');
+
+// // Arrays
+
+// const users = [{ name: 'Jean', email: 'jeanpipoxi@gmail.com' }];
+// console.log(users[0]?.name ?? 'User array empty');
+// console.log(users[1]?.name ?? 'User array empty');
+
 //* Enhanced object literals
+
+// in restaurant object
 
 //* Looping Arrays: The for-of loop
 
